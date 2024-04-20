7f224ec443f48360af5689295044ffa55e6761d8
Mehran Roham <mehranroham@gmail.com>
2024-04-20 23:54:51 +0330

    useffect added

--- src/components/AvailablePlaces.jsx
+++ src/components/AvailablePlaces.jsx
@@ -1,11 +1,52 @@
+import { useEffect, useState } from 'react';
 import Places from './Places.jsx';
+import Error from './Error.jsx';
 
 export default function AvailablePlaces({ onSelectPlace }) {
+  const [isFetching, setIsFetching] = useState(false);
+  const [availablePlaces, setAvailablePlaces] = useState([]);
+  const [error, setError] = useState();
+
+  useEffect(() => {
+    async function fetchPlaces() {
+      setIsFetching(true);
+      try {
+        const response = await fetch('http://localhost:3000/places');
+        const resData = await response.json();
+
+        if (!response.ok) {
+          const error = new Error('failed to fetch places');
+          throw error;
+          // ok 200 300
+          // !ok 400 500
+        }
+        setAvailablePlaces(resData.places);
+      } catch (error) {
+        setError({
+          message: error.message || 'could not fetch places, please try again',
+        });
+      }
+
+      setIsFetching(false);
+    }
+
+    fetchPlaces();
+  }, []);
+
+  if (error) {
+    return (
+      <>
+        <Error title='an error occurred!' message={error.message} />
+      </>
+    );
+  }
+
   return (
     <Places
-      title="Available Places"
-      places={[]}
-      fallbackText="No places available."
+      title='Available Places'
+      isFetching={isFetching}
+      places={availablePlaces}
+      fallbackText='No places available.'
       onSelectPlace={onSelectPlace}
     />
   );
--- src/components/Places.jsx
+++ src/components/Places.jsx
@@ -1,15 +1,26 @@
-export default function Places({ title, places, fallbackText, onSelectPlace }) {
-  console.log(places);
+export default function Places({
+  title,
+  places,
+  fallbackText,
+  onSelectPlace,
+  isFetching,
+}) {
   return (
-    <section className="places-category">
+    <section className='places-category'>
       <h2>{title}</h2>
-      {places.length === 0 && <p className="fallback-text">{fallbackText}</p>}
-      {places.length > 0 && (
-        <ul className="places">
+      {isFetching && <p className='fallback-text'>data is Fetching ...</p>}
+      {!isFetching && places.length === 0 && (
+        <p className='fallback-text'>{fallbackText}</p>
+      )}
+      {!isFetching && places.length > 0 && (
+        <ul className='places'>
           {places.map((place) => (
-            <li key={place.id} className="place-item">
+            <li key={place.id} className='place-item'>
               <button onClick={() => onSelectPlace(place)}>
-                <img src={`http://localhost:3000/${place.image.src}`} alt={place.image.alt} />
+                <img
+                  src={`http://localhost:3000/${place.image.src}`}
+                  alt={place.image.alt}
+                />
                 <h3>{place.title}</h3>
               </button>
             </li>
